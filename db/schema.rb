97d19ea45f99313b08f5aceb9ab3cb5aa5fba6c1
ryrbsd <ryrbsd@berkeley.edu>
2016-06-26 03:58:22 +0000

    add aplication navbar

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 20160623224754) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.boolean  "admin",                  default: false,  null: false
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
