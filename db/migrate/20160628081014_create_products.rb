ba5f8376056c0edf5b4a653a7375e79352d00512
xycheng0106@gmail.com <xycheng0106@gmail.com>
2016-07-04 05:17:35 +0000

    fixing upload image

--- db/migrate/20160628081014_create_products.rb
+++ db/migrate/20160628081014_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
       t.string :name
-      t.string :image
+      # t.string :image
       t.string :synopsis
       t.string :detail
       t.string :attachedFiles
---
+++ db/migrate/20160704045045_add_image_to_product.rb
@@ -0,0 +1,9 @@
+class AddImageToProduct < ActiveRecord::Migration
+  def self.up
+    add_column :products, :image, :string
+  end
+
+  def self.down
+    remove_column :products, :image
+  end
+end
