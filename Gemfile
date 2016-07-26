59b9a937fc12a844f5870fae98c045d907dec7c5
ryrbsd <ryrbsd@berkeley.edu>
2016-07-26 03:40:33 +0000

    fix bug

--- Gemfile
+++ Gemfile
@@ -51,6 +51,8 @@ gem "searchkick"
 gem 'elasticsearch-model' 
 gem 'elasticsearch-rails'
 
+gem 'jquery-turbolinks'
+
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require turbolinks
+
 //= require main
 //= require_tree .
