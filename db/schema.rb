d2a08bce0a7222621e072d462d7e12942a775db3
frankzhao3 <hpzhao3@berkeley.edu>
2016-08-08 05:06:56 +0000

    add fields to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808045305) do
+ActiveRecord::Schema.define(version: 20160808050117) do
 
   create_table "announcements", force: :cascade do |t|
     t.datetime "date"
@@ -231,6 +231,10 @@ ActiveRecord::Schema.define(version: 20160808045305) do
     t.datetime "updated_at",                               null: false
     t.string   "role",                   default: "admin"
     t.string   "authority"
+    t.string   "phoneNumber"
+    t.string   "company"
+    t.string   "department"
+    t.string   "realName"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
