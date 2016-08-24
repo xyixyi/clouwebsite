0772980f88cc52f349e758088698b1f50c2b5fb0
ryrbsd <ryrbsd@berkeley.edu>
2016-08-24 05:00:57 +0000

    aaa

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  
+  # for video might be useless
   config.assets.precompile += %w( video-js.swf vjs.eot vjs.svg vjs.ttf vjs.woff )
 end
