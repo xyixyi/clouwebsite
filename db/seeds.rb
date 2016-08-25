b7dee9f61738c648362270fdafed32434f7e1dc0
ryrbsd <ryrbsd@berkeley.edu>
2016-08-25 22:14:17 +0000

    fix login bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,7 @@ user.email = 'test@example.com'
 user.password = 'hangpeng123'
 user.password_confirmation = 'hangpeng123'
 user.role = 'developer'
+user.send_auth_email = false
 user.save!
 
 user2 = User.new
@@ -17,4 +18,5 @@ user2.email = 'test2@example.com'
 user2.password = 'hangpeng123'
 user2.password_confirmation = 'hangpeng123'
 user2.role = "admin"
+user.send_auth_email = false
 user2.save!
