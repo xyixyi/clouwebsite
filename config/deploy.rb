85ba737dbea26ec6778adc5e92e84105130b52a6
ryrbsd <ryrbsd@berkeley.edu>
2016-08-25 18:03:35 +0000

    add seed

--- config/deploy.rb
+++ config/deploy.rb
@@ -89,7 +89,11 @@ namespace :deploy do
         exit
       end
     end
-  end
+  desc "reload the database with seed data"
+  task :seed do
+    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+  end 
+end
 
   desc 'Initial Deploy'
   task :initial do
