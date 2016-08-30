f805b47af0b9d5356a206e76bce526b27f682731
xycheng0106@gmail.com <xycheng0106@gmail.com>
2016-08-30 04:24:00 +0000

    update crawler

--- crawler.rb
+++ crawler.rb
@@ -30,10 +30,11 @@ end
 
 url = 'http://vip.stock.finance.sina.com.cn/corp/view/vCB_AllBulletin.php?stockid=002121&Page='
 i = 1
+CSV.open("sina_link.csv", "w+") #清空原有的file
 while i <= 48 do
     doc = read_url(url + "#{i}")
     doc = doc.css('div.datelist')
-    CSV.open("cninfo.csv", "a+") do |csv|
+    CSV.open("sina_link.csv", "a+") do |csv|
         doc.css('a').each do |content|
             @title = content.text
             if @title.include? "股东大会"
@@ -57,19 +58,21 @@ puts "finish getting sina_link"
 puts "start geting result"
 
 # 读取原始csv，进入每个link找到pdf，重新存到新的csv
-
-csv = CSV.read('cninfo.csv')
+result = CSV.open("result.csv", "w+") #清空原有的file
+result << ["Date", "Title", "Link", "Type"]
+csv = CSV.read('sina_link.csv')
 csv.each do |row|
  doc = read_url(row[0])
  date = doc.css("td[style='text-align:center;height:12px;']").text[5..-1]
  pdf =  doc.css('tr th a')[0]['href']
- if !pdf.include? "PDF"
-    pdf = ""
+ if !pdf.include? "PDF" #如果新浪证券没有原始pdf文件，直接返回sina_link
+    pdf = row[0]
  end
- CSV.open("testest.csv", "a+") do |new_csv|
-    # sina_link, date, title, pdf_link, type
-    new_csv << [row[0], date, row[2], pdf, row[1]]
+
+ CSV.open("result.csv", "a+") do |new_csv|
+    # date, title, pdf_link, type
+    new_csv << [date, row[2], pdf, row[1]]
  end 
 end
 
-puts "finish. check result.csv"
+puts "finish. check result.csv and now you can import it"
