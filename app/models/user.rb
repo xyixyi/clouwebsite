99dfa6909b0b5f9a51b71525fea33511f19abe7c
frankzhao3 <hpzhao3@berkeley.edu>
2016-08-30 23:44:26 +0000

    changed

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
 
   # serialize :authority, Array
   def hashmap
-      { '产品类别' => 'Category, Type, Product, PopProduct','新闻' => 'CompanyNews, IndustryNews', 
+      { '产品类别' => 'Category, Type, Product, PopProduct','新闻' => 'CompanyNews, IndustryNews, SpecialNewsOne, SpecialNewsTwo, Honor', 
       '投资者关系' => 'Announcement, YearReport', '员工讯息' => 'EmployeeStory', '客户服务' => 'CaseType, ServiceCase, QType, QandA', 
       '招聘' => 'SocietyRecuritment, CampusRecuritment', '行业新闻' => 'IndustryNews' , '用户/管理员' => 'User', 
       '问题类型' => 'QType', '投标' => 'Bid', '售后服务' => 'Trouble, Complain'}
