11c2e9f413bb95a7daf8649a55f881c22b7a67df
ryrbsd <ryrbsd@berkeley.edu>
2016-08-31 06:35:48 +0000

    finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,7 +55,7 @@ class User < ActiveRecord::Base
         
         field :password do
           label "密码"
-          help "新增和更改密码时需填"
+          help "新增和更改密码时需填,后台编辑用户时不填则保留原密码"
         end
         
         field :realName do
