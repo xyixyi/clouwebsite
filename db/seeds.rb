260d980a442381483969a141f6291fe401572adc
ryrbsd <ryrbsd@berkeley.edu>
2016-09-12 08:38:26 +0000

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,29 +6,29 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user1 = User.new
-user1.email = 'raoyiran@hotmail.com'
-user1.password = 'aia20022'
-user1.password_confirmation = 'aia20022'
-user1.role = 'developer'
-user1.authorized = true
-user1.save!
+# user1 = User.new
+# user1.email = 'raoyiran@hotmail.com'
+# user1.password = 'aia20022'
+# user1.password_confirmation = 'aia20022'
+# user1.role = 'developer'
+# user1.authorized = true
+# user1.save!
 
-user3 = User.new
-user3.email = 'root@szclou.com'
-user3.password = 'rootszclou'
-user3.password_confirmation = 'rootszclou'
-user3.role = 'developer'
-user3.authorized = true
-user3.save!
-# Product.all.each do |product|
-#     if product.Authorized != true
-#         product.Authorized = true
-#         product.save!
-#     end
-# end
-# CompanyNews.all.each do |news|
-#     if news.Authorized != true
-#         news.Authorized = true
-#     end
-# end
+# user3 = User.new
+# user3.email = 'root@szclou.com'
+# user3.password = 'rootszclou'
+# user3.password_confirmation = 'rootszclou'
+# user3.role = 'developer'
+# user3.authorized = true
+# user3.save!
+Product.all.each do |product|
+    if product.Authorized != true
+        product.Authorized = true
+        product.save!
+    end
+end
+CompanyNews.all.each do |news|
+    if news.Authorized != true
+        news.Authorized = true
+    end
+end
